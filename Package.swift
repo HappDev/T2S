efcec8b058ce78f71d9c7166e983d770e30b885d
InfiniteFalltrough <golubenkovvs@gmail.com>
2025-02-15 01:10:26 +0700

    v0.0.8

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BadvpnTun2socks",
-            url: "https://github.com/FlyFrg/T2S/releases/download/v0.0.7/BadvpnTun2socks.xcframework.zip",
-            checksum: "5c588c43b2843bc4e20f0ffe62c0e24e9c1752af0dbaeab3e26fed067ebea71a"
+            url: "https://github.com/FlyFrg/T2S/releases/download/v0.0.8/BadvpnTun2socks.xcframework.zip",
+            checksum: "5fb0b50b682f6e7dea6b41626f31d7fcd1aed5241a6232b51638a7b94400dbd4"
         )
     ]
 )
